542994336702e77a4d6f4191c62049eb40035acc
Lyndon Duong <lyndon.duong@gmail.com>
2022-06-04 22:23:40 -0400

    update figure

--- figures/mercedes_schematic2.py
+++ figures/mercedes_schematic2.py
@@ -7,7 +7,9 @@ import seaborn as sns
 
 
 #%% simulate
-def simulate(Lxx, W, batch_size=64, n_batch=1024, lr_g=5e-3, g0=None, seed=None):
+def simulate(
+    cholesky_list, W, batch_size=64, n_batch=1024, lr_g=5e-3, g0=None, seed=None
+):
     if seed is not None:
         np.random.seed(seed)
 
@@ -16,30 +18,29 @@ def simulate(Lxx, W, batch_size=64, n_batch=1024, lr_g=5e-3, g0=None, seed=None)
     if g0 is not None:
         g = g0
     else:
-        g = np.random.rand(k)
+        g = fw.compute_g_opt(np.eye(n), W)  # initialize Parseval frame
 
     g_all = []
-    error = []
     # run simulation with minibatches
     responses = []
-    for _ in range(n_batch):
-        x = fw.sample_x(Lxx, batch_size)  # draw a sample of x
+    for Lxx in cholesky_list:
+        for _ in range(n_batch):
+            x = fw.sample_x(Lxx, batch_size)  # draw a sample of x
 
-        # steady-state of y
-        y = np.linalg.inv(W @ np.diag(g) @ W.T) @ x
+            # steady-state of y
+            y = np.linalg.inv(W @ np.diag(g) @ W.T) @ x
 
-        # compute z and descend gradient of L(g,y) wrt g
-        z = W.T @ y
-        # dg = 1/batch_size * np.diag(z @ z.T) - np.diag(W @ W.T)
-        dg = np.mean(z**2, -1) - 1.0  # more efficient, local gradient
-        g = g + lr_g * dg  # gradient descent
+            # compute z and descend gradient of L(g,y) wrt g
+            z = W.T @ y
+            # dg = 1/batch_size * np.diag(z @ z.T) - np.diag(W @ W.T)
+            dg = np.mean(z**2, -1) - 1.0  # more efficient, local gradient
+            g = g + lr_g * dg  # gradient descent
 
-        # Cyy = np.cov(y)
-        responses.append((x.mean(-1), y.mean(-1), z.mean(-1)))
-        g_all.append(g)
-    error = np.array(error)
+            # Cyy = np.cov(y)
+            responses.append((x.mean(-1), y.mean(-1), z.mean(-1)))
+            g_all.append(g)
     g_all = np.stack(g_all, 0)
-    return g, g_all, error, responses
+    return g, g_all, responses
 
 
 #%%
@@ -48,17 +49,19 @@ np.random.seed(420)
 num_switches = 2
 n, k = 2, 3
 batch_size = 1
-n_batch = 500
-lr_g = 5e-3
-# Cxx, Lxx = fw.randcov2()
+n_batch = 750
+lr_g = 3e-3
+
 V, _ = np.linalg.qr(np.random.randn(n, n))
-Cxx = V @ np.diag([3, 1]) @ V.T * np.random.rand() * 2
-Lxx = np.linalg.cholesky(Cxx)
+Cxx0 = V @ np.diag([3, 1]) @ V.T * 0.1
+V, _ = np.linalg.qr(np.random.randn(n, n))
+Cxx1 = V @ np.diag([3, 1]) @ V.T * 0.5
+cholesky_list = [np.linalg.cholesky(C) for C in (Cxx0, Cxx1)]
 W = fw.get_mercedes_frame()
 
 # g0 = np.random.rand(k) * 1
 g0 = np.array([0.01, 1.2, 0.25])
-g, g_all, error, responses = simulate(Lxx, W, batch_size, n_batch, lr_g, g0=g0)
+g, g_all, responses = simulate(cholesky_list, W, batch_size, n_batch, lr_g, g0=g0)
 
 #%%
 plt.plot(g_all)
@@ -78,32 +81,65 @@ def moving_average(x, w):
     return y
 
 
-t_skip = 1
+t_skip = 5
 X0 = X[::t_skip, :]
 Y0 = Y[::t_skip, :]
 Y0 = Y[::t_skip, :]
 
 sz2 = Z**2
-sz2 = moving_average(sz2, 25)
+sz2 = moving_average(sz2, 15)
 sz2 = sz2[::t_skip, :]
-T = len(X0)
+T = len(X)
 
 cols = sns.color_palette("husl", 3)
+ctx_cols = sns.color_palette("Set1", 2)  # context
+slice_cols0 = sns.color_palette("light:red", 10)[1:4]
+slice_cols1 = sns.color_palette("light:blue", 10)[1:4]
+
+slice_cols = np.concatenate((slice_cols0, slice_cols1))
+
 with sns.plotting_context("paper", font_scale=1.5):
-    fig, ax = plt.subplots(5, 1, figsize=(5, 10), sharex="all", dpi=300)
-    ax[0].scatter(range(T), X0[:, 0], s=1)
-    ax[1].scatter(range(T), X0[:, 1], s=1)
-    [ax[2].plot(range(T), sz2[:, i], color=cols[i], linewidth=3) for i in range(k)]
-    ax[2].hlines(1, 0, T, linestyle="--", color="k")
+    fig, ax = plt.subplots(5, 1, figsize=(5, 8), sharex="all", dpi=300)
+    size = 5
+    time = np.arange(0, T, t_skip)
 
-    ax[-2].scatter(range(T), Y0[:, 0], s=1, color="k")
-    ax[-1].scatter(range(T), Y0[:, 1], s=1, color="k")
+    for i in range(5):
+        ax[i].vlines(n_batch, -5, 5, linestyle="-", color="k")
 
-    ax[0].set(ylabel=r"$x_0$")  # , xlim=(0, 1000))
-    ax[1].set(ylabel=r"$x_1$")
-    ax[2].set(ylabel=r"$\sigma^2_z$", ylim=(1e-1, 5), yscale="linear")
-    ax[-2].set(ylabel=r"$y_0$")
-    ax[-1].set(ylabel=r"$y_1$", xlabel="Step")
+    # contexts
+    rectangle0 = plt.Rectangle((0, 2.5), n_batch, 1, fc=ctx_cols[0], ec="none")
+    rectangle1 = plt.Rectangle((n_batch, 2.5), n_batch, 1, fc=ctx_cols[1], ec="none")
+    ax[0].add_patch(rectangle0)
+    ax[0].add_patch(rectangle1)
+
+    # x inputs
+    ax[0].scatter(time, X0[:, 0], s=size, c="darkgrey")
+    ax[1].scatter(time, X0[:, 1], s=size, c="darkgrey")
+
+    # z variance
+    [ax[2].plot(time, sz2[:, i], color=cols[i], linewidth=2) for i in range(k)]
+    ax[2].hlines(1, 0, time[-1], linestyle="--", color="k")
+
+    # time slices
+    for i, tt in enumerate((0, 100, 650, 750, 850, 1400)):
+        for j in (-2, -1):
+            rect = plt.Rectangle((tt, -5), 75, 10, fc=slice_cols[i], ec="none")
+            ax[j].add_patch(rect)
+
+    # y outputs
+    ax[-2].scatter(time, Y0[:, 0], s=size, color="k")
+    ax[-1].scatter(time, Y0[:, 1], s=size, color="k")
+
+    ylim_x = (-3, 3)
+    ylim_y = (-3, 3)
+    yticks_x = (-3, 0, 3)
+    yticks_y = (-3, 0, 3)
+    ax[0].set(ylabel=r"$x_0$", ylim=ylim_x, yticks=yticks_x, xlim=(0, T))
+    ax[1].set(ylabel=r"$x_1$", ylim=ylim_x, yticks=yticks_x)
+    ax[2].set(ylabel=r"$\sigma^2_z$", ylim=(1e-1, 10), yscale="log")
+    ax[-2].set(ylabel=r"$y_0$", ylim=ylim_y, yticks=yticks_y)
+    ax[-1].set(ylabel=r"$y_1$", xlabel="Step", ylim=ylim_y, yticks=yticks_y)
+    ax[-1].set(xticks=(0, 750, 1500))
 
     sns.despine()
 
@@ -116,31 +152,34 @@ def get_Cyy(Cxx, W, g):
     return Cyy
 
 
-t0, t1, t2 = 0, 75, 300
-Cyy0 = get_Cyy(Cxx, W, g_all[t0])
-Cyy1 = get_Cyy(Cxx, W, g_all[t1])
-Cyy2 = get_Cyy(Cxx, W, g_all[t2])
-
+In = np.eye(2)
 with sns.plotting_context("paper", font_scale=1.5):
-    fig, ax = plt.subplots(1, 3, figsize=(6, 3), sharex="all", sharey="all")
-    In = np.eye(2)
-    I_kwargs = {"linestyle": "--", "color": "C3"}
+    fig_dims = (1, 6)
+    fig, ax = plt.subplots(*fig_dims, figsize=(6, 5), dpi=300, squeeze=False)
+
+    I_kwargs = {"linestyle": "--", "linewidth": 2, "color": "w"}
     Cyy_kwargs = {"linewidth": 2, "color": "k"}
-    fwplt.plot_ellipse(Cyy0, ax=ax[0], **Cyy_kwargs)
-    fwplt.plot_ellipse(In, ax=ax[0], **I_kwargs)
+    t_slice = (0, 100, n_batch, n_batch + 1, n_batch + 100, -1)
+    for i in range(len(t_slice)):
+        idx = np.unravel_index(i, fig_dims)
+        ctx = i > 2
+        Lxx = cholesky_list[ctx]
 
-    fwplt.plot_ellipse(Cyy1, ax=ax[1], **Cyy_kwargs)
-    fwplt.plot_ellipse(In, ax=ax[1], **I_kwargs)
+        t0 = t_slice[i]
+        Cxx = Lxx @ Lxx.T
+        Cyy = get_Cyy(Cxx, W, g_all[t0])
 
-    fwplt.plot_ellipse(Cyy2, ax=ax[2], **Cyy_kwargs)
-    fwplt.plot_ellipse(In, ax=ax[2], **I_kwargs)
-
-    ax[0].axis("square")
-    ax[1].axis("square")
-    ax[2].axis("square")
-    lim_sq = 10
-    ax[0].set(xlim=(-lim_sq, lim_sq), ylim=(-lim_sq, lim_sq), title="t=0")
-    ax[1].set(title=f"t={t1}")
-    ax[2].set(title=f"t=end")
-
+        fwplt.plot_ellipse(Cyy, ax=ax[idx], **Cyy_kwargs)
+        fwplt.plot_ellipse(In, ax=ax[idx], **I_kwargs)
+        lim_sq = 5
+        idx = np.unravel_index(i, fig_dims)
+        ax[idx].axis("square")
+        ax[idx].set(xlim=(-lim_sq, lim_sq), ylim=(-lim_sq, lim_sq))
+        rect = plt.Rectangle((-5, -5), 10, 10, fc=slice_cols[i], ec="none")
+        ax[idx].add_patch(rect)
+        if i > 0:
+            ax[idx].set(xticklabels=[], yticklabels=[])
+    ax[0, 0].set(xlabel=r"$y_0$", ylabel=r"$y_1$", xticks=(-5, 0, 5), yticks=(-5, 0, 5))
     sns.despine()
+
+# %%
