110c23b6c5995ec4b6e4bb52ee144d0dc524735b
Lyndon Duong <lyndon.duong@gmail.com>
2022-06-05 22:49:15 -0400

    non-neg constraint figure

--- experiments/varying_alpha.py
+++ experiments/varying_alpha.py
@@ -4,6 +4,7 @@ June 06, 2022
 Seems to increase in convergence speed for with increasing alpha
 """
 
+from turtle import bgcolor
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -77,13 +78,14 @@ np.random.seed(420)
 n_contexts = 2
 n, k = 2, 3
 batch_size = 256
-n_batch = 20000
-lr_g = 1e-4
+n_batch = 5000
+lr_g = 1e-3
 
 # V, _ = np.linalg.qr(np.random.randn(n, n))
 # Cxx0 = V @ np.diag([3.5, 1]) @ V.T * 0.1
 Q = fw.rot2(np.deg2rad(45))
-Cxx0 = Q @ np.diag([20, 1]) @ Q.T * 1.0
+kappa = 8
+Cxx0 = Q @ np.diag([20, 1]) @ Q.T * 1 / (np.sqrt(kappa))
 
 cholesky_list = [np.linalg.cholesky(C) for C in [Cxx0]]
 W = fw.get_mercedes_frame()
@@ -91,13 +93,14 @@ W = fw.get_mercedes_frame()
 # W = np.concatenate([W, np.array([[np.cos(tx)], [np.sin(tx)]])], -1)
 # W = fw.normalize_frame(np.random.randn(n, 4))
 
-alphas = [0.0, 1.0, 2.0, 3.0, 5.0, 6.0]
+# alphas = [0.0, 1.0, 2.0, 3.0, 5.0, 6.0]
 # alphas = [2.0]
-# alphas = [0.0, 1.0]
+alphas = [0.0, 1.0]
 all_gs = []
 all_g_last = []
 with sns.plotting_context("paper", font_scale=1.5):
-    fig, ax = plt.subplots(1, 1, figsize=(8, 4), dpi=150)
+    sns.set_style("white")
+    fig, ax = plt.subplots(1, 1, figsize=(8, 4), dpi=300)
     cols = sns.color_palette("mako", len(alphas))
     N, K = W.shape
     # g0 = np.random.randn(K)
@@ -123,18 +126,43 @@ with sns.plotting_context("paper", font_scale=1.5):
         # ylim=(1e-4, 1e0),
         xlim=(0, n_batch),
         xlabel="iteration",
-        ylabel=r"$\frac{1}{N}$ Tr($\vert {\bf C}_{yy} - {\bf I} \vert$)",
+        ylabel=r"Error: $\frac{1}{N}$ Tr($\vert {\bf C}_{yy} - {\bf I} \vert$)",
         title=f"$\eta_g=${lr_g:.2e}, batch_size={batch_size}",
     )
     ax.legend(title=r"$\alpha$")
     sns.despine()
 
-fig, ax = plt.subplots(1, 1)
-fwplt.plot_ellipse(Cxx0, ax=ax)
-fwplt.plot_frame2d(W, ax=ax)
-ax.axis("square")
-ax.set(xlim=(-4, 4), ylim=(-4, 4), ylabel=r"$y_1$", xlabel=r"$y_0$")
-sns.despine()
+#%%
+
+cols = sns.color_palette("mako", len(alphas))
+with sns.plotting_context("paper", font_scale=1):
+    sns.set_style("dark")
+    fig, ax = plt.subplots(1, 1, figsize=(6, 6), dpi=300)
+    M0 = np.linalg.inv(W @ np.diag(all_g_last[0]) @ W.T)
+    M1 = np.linalg.inv(W @ np.diag(all_g_last[1] ** 2) @ W.T)
+    fwplt.plot_ellipse(Cxx0, ax=ax, **{"color": "k", "linewidth": 3, "label": r"Init"})
+    fwplt.plot_ellipse(
+        M0 @ Cxx0 @ M0,
+        ax=ax,
+        **{"color": cols[0], "linewidth": 3, "label": r"Whitened, $\alpha=0$"},
+    )
+    fwplt.plot_ellipse(
+        M1 @ Cxx0 @ M1,
+        ax=ax,
+        **{"color": cols[1], "linewidth": 3, "label": r"Whitened, $\alpha=1$"},
+    )
+    fwplt.plot_ellipse(
+        np.eye(n),
+        ax=ax,
+        **{"color": "w", "linewidth": 2, "linestyle": "--", "label": "Identity"},
+    )
+
+    fwplt.plot_frame2d(W, ax=ax)
+    ax.legend(title=r"${\bf C}_{yy}$")
+
+    ax.axis("square")
+    ax.set(xlim=(-4, 4), ylim=(-4, 4), ylabel=r"$y_1$", xlabel=r"$y_0$")
+    sns.despine()
 
 #%%
 Cxx_sqrt = fw.psd_sqrt(Cxx0)
@@ -145,15 +173,16 @@ print(r"optimal $g^{\alpha+1}$", g_opt)
 
 #%%
 with sns.plotting_context("paper", font_scale=1.5):
-    fig, ax = plt.subplots(1, 2, figsize=(8, 4), dpi=150, sharex="all", sharey="all")
+    sns.set_style("white")
+    fig, ax = plt.subplots(1, 2, figsize=(10, 6), dpi=300, sharex="all", sharey="all")
     ax[0].plot(all_gs[0])
-    # ax[0].hlines(g_opt, 0, n_batch, linestyle="--")
+    ax[0].hlines(g_opt, 0, n_batch, linestyle="--")
 
     i = 1
     ax[1].plot(all_gs[i] ** (alphas[i] + 1))
-    # ax[1].hlines(g_opt, 0, n_batch, linestyle="--")
+    ax[1].hlines(g_opt, 0, n_batch, linestyle="--")
 
-    ax[0].set(ylabel=r"$g^{\alpha + 1}$", title=r"$\alpha=0$")
+    ax[0].set(ylabel=r"$g^{\alpha + 1}$", title=r"$\alpha=0$", xlabel="Iter")
     ax[1].set(title=r"$\alpha=1$")
     sns.despine()
 
