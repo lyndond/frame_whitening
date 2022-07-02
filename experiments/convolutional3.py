95c56082277869fe4e6982f53ec7a0b50c5f7717
Lyndon Duong <lyndon.duong@gmail.com>
2022-07-02 01:04:35 -0400

    local conv whitening by david

--- experiments/convolutional3.py
+++ experiments/convolutional3.py
@@ -6,7 +6,6 @@ From David's note.
 Weights are shared across z's.
 June 30, 2022
 """
-
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -17,41 +16,39 @@ import scipy as sp
 
 np.random.seed(1)
 
-# construct a convolutional filter bank (plotted below)
-w_vec = np.random.randn(4)
-W_hat_transpose = sp.linalg.toeplitz(w_vec)  # make circulant matrix
-W_hat_transpose = W_hat_transpose[:, :-1]  # drop last column
-# remove corners
-W_hat_transpose[-1][0] = 0
-W_hat_transpose[0][-1] = 0
-W_hat = W_hat_transpose.T  # flip it
+N = 5
+x_dim = N
+y_dim = N
+z_dim = 2 * N - 1
 
-# generate Cxx
-N = 3
-# construct a local banded Cxx
-Cxx = np.array(
-    (
-        [2.0, -1, 0.5],
-        [-1, 2.0, -1],
-        [0.5, -1, 2.0],
-    )
-)
+W_hat = np.zeros((y_dim, z_dim))
+
+for i in range(z_dim):
+    if i < y_dim:
+        W_hat[i, i] = 1
+    else:
+        W_hat[i - y_dim, i] = 1
+        W_hat[i - y_dim + 1, i] = 1
 
 # random Cxx
-# A = np.random.randn(N, N)
-# Cxx = A @ A.T
+A = np.random.randn(N, N)
+Cxx = A @ A.T
+
+u, s, vh = np.linalg.svd(Cxx)
+
+A = u @ np.diag(np.sqrt(s)) @ vh
 
 T = 512  # batch size
 n_batch = int(2**13)
 norm_w_sq = np.diag(W_hat.T @ W_hat)
 eta_g = 1e-3
-g = np.ones(N + 1)  # init g
+g = np.ones(z_dim)  # init g
 all_g = []
 error = []
 
 # run simulation
 for _ in tqdm(range(n_batch)):
-    x = A @ np.random.randn(N, T)
+    x = A @ np.random.randn(x_dim, T)
     M = W_hat @ np.diag(g) @ W_hat.T
     M_inv = np.linalg.inv(M)
     y = M_inv @ x
