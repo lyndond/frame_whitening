64b39a5ad979ce25988af068ab813d04ce56692c
Lyndon D <lyndon.duong@gmail.com>
2022-11-25 08:40:04 -0500

    docstring and typing

--- frame_whitening/data2.py
+++ frame_whitening/data2.py
@@ -1,23 +1,69 @@
+import array
+import os
+import os.path as op
+from typing import Optional,Tuple
+
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 
 
-def random_walk(n_steps, sigma=1.0):
-    # 2d gaussian random walk
+def load_image(
+    data_dir: str = "/mnt/home/tyerxa/ceph/datasets/datasets/vanhateren_imc", 
+    crop_size: int = 256, 
+    rng: Optional[np.random.Generator] = None,
+    ) -> npt.NDArray[np.uint16]:
+    """Loads randomly cropped img from van hateren dataset."""
+
+    if rng is None:
+        rng = np.random.default_rng()
+
+    files = sorted(os.listdir(data_dir))
+    n_images = 10
+    rand_idx = rng.choice(range(len(files)), n_images, replace=False)
+
+    filename = files[0]
+    with open(op.join(data_dir, filename), 'rb') as handle:
+        s = handle.read()
+        arr = array.array('H', s)
+        arr.byteswap()
+    img = np.array(arr, dtype='uint16').reshape(1024, 1536)
+    H, W = img.shape
+
+    rand_h = rng.integers(0, H-crop_size, 1)[0]
+    rand_w = rng.integers(0, W-crop_size, 1)[0]
+    img = img[rand_h:rand_h + crop_size, rand_w:rand_w + crop_size]
+    return img
+
+
+def random_walk(n_steps: int, sigma: float = 1.0) -> Tuple[npt.NDArray[np.int], npt.NDArray[np.int]]:
+    """2D Gaussian random walk."""
     x = np.random.normal(0, sigma, n_steps)
     y = np.random.normal(0, sigma, n_steps)
     return np.cumsum(x).astype(int), np.cumsum(y).astype(int)
 
 
-def get_patches(img, walk_h, walk_w):
-    # get patches within a context
+def get_patches(
+    img: npt.NDArray, 
+    walk_h: npt.NDArray[np.int], 
+    walk_w: npt.NDArray[np.int]
+    ) -> List[npt.NDArray]:
+    """Get patches within a context."""
     all_images = []
     for di, dj in zip(walk_h, walk_w):
         all_images.append(img[di:di+h, dj:dj+w])
     return all_images
 
 
-def get_contexts(img, h, w, n_contexts, sigma, n_steps, pad_factor=1):
+def get_contexts(
+    img: npt.NDArray, 
+    h: int, 
+    w: int, 
+    n_contexts: int, 
+    sigma: float, 
+    n_steps: int, 
+    pad_factor: int = 1
+    ) -> Tuple[npt.NDArray, npt.NDArray[np.int]]:
     pad_h, pad_w = pad_factor * h, pad_factor * w
 
     all_contexts = []
@@ -59,7 +105,7 @@ def add_subplot_border(ax, width=1, color=None ):
     fig.patches.append(rect)
 
 
-def plot_context_samples(all_contexts, n_samples):
+def plot_context_samples(all_contexts: npt.NDArray, n_samples: int):
     n_contexts = all_contexts.shape[0]
     sampled_idx = np.random.choice(n_steps, n_samples, replace=False)
     fig, ax = plt.subplots(n_contexts, n_samples, figsize=(n_samples, 4))
@@ -73,7 +119,7 @@ def plot_context_samples(all_contexts, n_samples):
     fig.tight_layout()
 
 
-def plot_patch_stats(all_images):
+def plot_patch_stats(all_images: npt.NDArray) -> None:
     fig, ax = plt.subplots(1, 2, figsize=(10, 4), sharex="all", sharey="all")
 
     im = ax[0].imshow(np.mean(all_images, 0), cmap="bone")
@@ -84,3 +130,4 @@ def plot_patch_stats(all_images):
     ax[1].set(title="Cross-patch variance", xticklabels=[], yticklabels=[])
     plt.colorbar(im)
     fig.tight_layout()
+
